61c57fed35e02a92b2c084db31a32329f88757ef
richardgorham1 <richardgorham1@cox.net>
2018-08-17 08:59:45 -0700

    second

--- string-match.py
+++ string-match.py
@@ -5,15 +5,15 @@ from fuzzywuzzy import process
 
 welcome = pd.read_csv('./WELLCOME_APCspend2013_forThinkful.csv',encoding='iso8859_15')
 
-def simularities(needle, haystack):
+def similarities(needle, haystack):
     return process.extract(needle, haystack)[0]
 
 j_titles_1 = pd.DataFrame(welcome['Journal title'].unique())
 j_titles_1.rename(index=int, columns={0:'title'},inplace=True)
 
-titles_pubmed = pd.read_csv('./titles_pubmed.txt', delimiter=';', header=None,
-                             names=['abbreviation', 'title'])
-
-for i in j_titles_1.title.head(5):
-    result = simularities(i, titles_pubmed.title)
-    print(i, result)
+titles_pubmed = pd.read_csv('./titles_pubmed.txt', delimiter=';', header=None, names=['abbreviation', 'title'])
+for i in j_titles_1.title:
+    data = similarities(i, titles_pubmed.title)
+    abbr = titles_pubmed.loc[titles_pubmed['title'] == similarities(i, titles_pubmed.title)[0], 'abbreviation']
+    abbr = list(abbr)
+    print([i, data, abbr])
